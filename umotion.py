3a311a67145e7ccee6d465b4950cbb9c7d71e9b6
Greg <gnfrazier@gmail.com>
2017-02-28 20:55:54 -0500

    Add support for config.json file.

--- umotion.py
+++ umotion.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
 
 from machine import Pin
+import cfg
+import msg
 
-def get_pin():
-    pass
 
 def setup_sensor(pin):
     motion = Pin(pin, Pin.IN)
-    
+
     return motion
 
+
+def wait_detection(c, motion):
+    detections = 0
+    server = c['server']
+    topic = c['ptopic']
+    while detections < 10:
+        if motion.value():
+            detections += 1
+            msg.xmt(server, topic, 'motion detected')
+            msg.xmt(server, topic, str(detections))
+        else:
+            pass
+
+
 def main():
-    pin = 1
+    c = cfg.read_cfg()
+    pin = int(c['pin'])
     motion = setup_sensor(pin)
+    wait_detection(c, motion)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
