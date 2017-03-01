d3878fe0b4e9c2092738c408a0cf7ff1a36b89e9
Greg <gnfrazier@gmail.com>
2017-02-28 21:24:16 -0500

    Added delay to config for delay after detection.

--- umotion.py
+++ umotion.py
@@ -3,6 +3,7 @@
 from machine import Pin
 import cfg
 import msg
+import time
 
 
 def setup_sensor(pin):
@@ -15,13 +16,22 @@ def wait_detection(c, motion):
     detections = 0
     server = c['server']
     topic = c['ptopic']
-    while detections < 10:
-        if motion.value():
-            detections += 1
-            msg.xmt(server, topic, 'motion detected')
-            msg.xmt(server, topic, str(detections))
-        else:
-            pass
+    delay = int(c['delay'])
+    try:
+        while detections < 10:
+            if motion.value():
+                detections += 1
+                msg.xmt(server, topic, 'motion detected')
+                msg.xmt(server, topic, str(detections))
+                time.sleep(delay)
+            else:
+                pass
+    except Exception as e:
+        # log.debug(e)
+        #mem = gc.mem_alloc()
+        # log.debug(mem)
+        gc.collect()
+        time.sleep(1)
 
 
 def main():
